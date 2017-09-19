995ba875a6229e8bfe64fb973d71d305e61eb794
cnhnnyzhy <cnhnnyzhy@163.com>
2017-09-19 18:07:57 +0800

    配置日志级别

--- springboot-demo/src/main/java/com/zhy/java/springboot/SpringbootDemoApplication.java
+++ springboot-demo/src/main/java/com/zhy/java/springboot/SpringbootDemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.zhy.java.springboot.dao")
 @SpringBootApplication
 public class SpringbootDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
