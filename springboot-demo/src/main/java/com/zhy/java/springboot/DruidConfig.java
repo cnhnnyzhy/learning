376932509af48558e5224598090a5e073930a9e4
cnhnnyzhy <cnhnnyzhy@163.com>
2017-09-20 13:44:49 +0800

    增加druid连接池支持

---
+++ springboot-demo/src/main/java/com/zhy/java/springboot/DruidConfig.java
@@ -0,0 +1,125 @@
+package com.zhy.java.springboot;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+public class DruidConfig {
+    private Logger logger = LoggerFactory.getLogger(DruidConfig.class);
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+    @Value("${spring.datasource.filters}")
+    private String filters;
+    @Value("${spring.datasource.logSlowSql}")
+    private String logSlowSql;
+    @Value("${spring.datasource.connectionProperties}")
+    private String connectionProperties;
+    @Bean
+    public ServletRegistrationBean druidStatViewServlet(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny","192.168.1.73");
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername","admin");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean druidStatFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    @Primary  //表示这里定义的DataSource将覆盖其他来源的DataSource
+    public DataSource druidDataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnReturn(testOnReturn);
+        dataSource.setPoolPreparedStatements(poolPreparedStatements);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        //添加其它配置属性
+        if(connectionProperties != null && !"".equals(connectionProperties)){
+            String[] properties = connectionProperties.split(";");
+            if(properties != null && properties.length > 0){
+                for (String property : properties){
+                    if(property != null && !"".equals(property)){
+                        String[] keyAndValue = property.split("=");
+                        if(keyAndValue != null && keyAndValue.length == 2){
+                            dataSource.addConnectionProperty(keyAndValue[0], keyAndValue[1]);
+                        }else{
+                            logger.error("connectionProperties config error: property must be split by '='");
+                        }
+                    }
+                }
+            }
+        }
+        try {
+            dataSource.setFilters(filters);
+        } catch (SQLException e) {
+            logger.error("druid configuration initialization filter", e);
+        }
+        return dataSource;
+    }
+}
--- springboot-demo/src/main/java/com/zhy/java/springboot/dao/IOrdInfoDao.java
+++ springboot-demo/src/main/java/com/zhy/java/springboot/dao/IOrdInfoDao.java
@@ -2,6 +2,7 @@ package com.zhy.java.springboot.dao;
 
 import com.ifec.blueair.framework.dao.BaseDAO;
 import com.zhy.java.springboot.model.OrdInfo;
+import org.apache.ibatis.annotations.Mapper;
 public interface IOrdInfoDao extends BaseDAO<OrdInfo> {
 	
 }
