21a8aabfbc5ee356eb60e5f736e4439cc30da1ba
cnhnnyzhy <cnhnnyzhy@163.com>
2017-10-18 00:32:36 +0800

    windows系统文件夹大小统计

--- algorithm/src/main/java/com/zhy/java/algorithm/WindowsDirectorySizeStatistics.java
+++ algorithm/src/main/java/com/zhy/java/algorithm/WindowsDirectorySizeStatistics.java
@@ -1,9 +1,11 @@
 package com.zhy.java.algorithm;
 
 import java.io.File;
+import java.text.DecimalFormat;
 import java.text.MessageFormat;
 
 public class WindowsDirectorySizeStatistics {
+    public static final DecimalFormat df = new DecimalFormat("#.##");
     private String dirPath;
     private long gtSize;
 
@@ -12,64 +14,62 @@ public class WindowsDirectorySizeStatistics {
         this.gtSize = gtSize;
     }
 
-    public void statistics(){
+    public void statistics() {
         File dirFile = new File(dirPath);
-        if(!dirFile.exists()){
-            System.out.println("目录["+dirPath+"]不存在！");
+        if (!dirFile.exists()) {
+            System.out.println("目录[" + dirPath + "]不存在！");
             return;
         }
-        if(!dirFile.isDirectory()){
-            System.out.println("["+dirPath+"]不是目录！");
+        if (!dirFile.isDirectory()) {
+            System.out.println("[" + dirPath + "]不是目录！");
             return;
         }
         File[] childFiles = dirFile.listFiles();
-        if(childFiles == null || childFiles.length <= 0){
-            System.out.println("目录["+dirPath+"]为空！");
+        if (childFiles == null || childFiles.length <= 0) {
+            System.out.println("目录[" + dirPath + "]为空！");
             return;
         }
         long size;
-        for (File childFile : childFiles){
-            if(childFile.isDirectory()){
+        for (File childFile : childFiles) {
+            if (childFile.isDirectory()) {
                 size = statisticsDir(childFile);
-            }else{
+            } else {
                 size = childFile.length();
             }
-            if(gtSize > 0 && size > gtSize) {
+            if (gtSize > 0 && size > gtSize) {
                 System.out.println(MessageFormat.format("{0}[{1}]", childFile.getPath(), transFileSize(size)));
             }
         }
 
     }
 
-    private long statisticsDir(File dirFile){
+    private long statisticsDir(File dirFile) {
         long size = 0;
         File[] files = dirFile.listFiles();
-        if(files == null){
+        if (files == null) {
             return size;
         }
-        for (File file : files){
-            if(file.isDirectory()){
+        for (File file : files) {
+            if (file.isDirectory()) {
                 size += statisticsDir(file);
-            }else{
+            } else {
                 size += file.length();
             }
         }
         return size;
     }
 
-    private String transFileSize(long size){
-        long t_kb = size / 1024;
-        long t_mb = t_kb / 1024;
-        long kb = t_kb % 1024;
-        long t_gb = t_mb / 1024;
-        long mb = t_mb % 1024;
+    private String transFileSize(long size) {
+        double kb = size / 1000;
+        double mb = kb / 1024;
+        double gb = kb / (1024 * 1024);
         StringBuffer sb = new StringBuffer();
-        if(t_gb > 0){
-            sb.append(t_gb).append("G");
-        }else if(mb > 0){
-            sb.append(mb).append("M");
-        }else {
-            sb.append(kb).append("K");
+        if (gb >= 1) {
+            sb.append(df.format(gb)).append("G");
+        } else if (mb >= 1) {
+            sb.append(df.format(mb)).append("M");
+        } else {
+            sb.append(df.format(kb)).append("K");
         }
         return sb.toString();
     }
@@ -77,8 +77,10 @@ public class WindowsDirectorySizeStatistics {
     public static void main(String[] args) {
         String dirPath = "D:\\IdeaProjects\\learning\\java-interview";
         dirPath = "c:/";
-        dirPath = "c:\\Windows";
-        long gtSize = 1 * 1024 * 1024 * 1000;
+        //dirPath = "c:\\Windows";
+        dirPath = "c:\\Users\\yang.zhang3\\AppData\\Local";
+        dirPath = "d:/";
+        long gtSize = 1 * 1024 * 1000;
         WindowsDirectorySizeStatistics windowsDirectorySizeStatistics = new WindowsDirectorySizeStatistics(dirPath, gtSize);
         windowsDirectorySizeStatistics.statistics();
     }
