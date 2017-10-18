ef081dc2f454691c63207315c602da1240842de2
cnhnnyzhy <cnhnnyzhy@163.com>
2017-10-18 18:13:43 +0800

    windows文件大小统计工具

---
+++ windows-directory/src/main/java/com/zhy/java/tools/windowsdirectory/WindowsDirectoryApplication.java
@@ -0,0 +1,12 @@
+package com.zhy.java.tools.windowsdirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowsDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowsDirectoryApplication.class, args);
+	}
+}
---
+++ windows-directory/src/test/java/com/zhy/java/windowsdirectory/WindowsDirectoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhy.java.windowsdirectory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WindowsDirectoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
